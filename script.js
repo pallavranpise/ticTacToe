b8f7572da53c43800ba56316865992f85ba2de80
pallavranpise <runpsaypallove@gmail.com>
2022-09-27 14:05:04 +0530

    small changes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const box = document.querySelectorAll(".box");
+const gameLogic = (()=>{
+
+  const box = document.querySelectorAll(".box");
+  let turnXonEven = 1;
+  
+Array.from(box).forEach((element) => {
+  element.addEventListener("click", () => {
+    if (turnXonEven > 9) clearGame();
+    if (!element.classList.contains("ring") && !element.classList.contains("cross"))
+      {
+        turnXonEven++;
+        element.classList.add(turn());
+      }
+    });
+  });
+  
+  let turn = () => (turnXonEven % 2 === 0 ? "ring" : "cross");
+
+
+let clearGame = function () {
+  turnXonEven = 1;
+  Array.from(box).forEach((element) => {
+    element.classList.remove("ring", "cross");
+  });
+};
+
+})()
+
